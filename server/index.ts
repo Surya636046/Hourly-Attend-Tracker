58f01a07082d06cf7ddab86c63c893218aab94b9
Surya636046 <727824tucs425@skct.edu.in>
2026-03-08 11:10:45 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -90,8 +90,9 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  app.listen(5000, "localhost", () => {
-  console.log("Server running on http://localhost:5000");
+  const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 })();
